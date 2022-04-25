a5b48f30290661f3cb5e98b1f26cf71715e51497
jash <jash2212004@gmail.com>
2022-04-26 01:38:26 +0530

    Updates

---
+++ precache-manifest.be4a2b3e792c837d19a0e8f5efc26d65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436a494db1dbd0e81961e2abc46bfb30",
+    "url": "/D-Cloud/index.html"
+  },
+  {
+    "revision": "463845310e40a58b2322",
+    "url": "/D-Cloud/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "7711e06160144b588040",
+    "url": "/D-Cloud/static/css/main.a22aaf15.chunk.css"
+  },
+  {
+    "revision": "463845310e40a58b2322",
+    "url": "/D-Cloud/static/js/2.21ce7114.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/D-Cloud/static/js/2.21ce7114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7711e06160144b588040",
+    "url": "/D-Cloud/static/js/main.b1a8d8f6.chunk.js"
+  },
+  {
+    "revision": "298d8876c293b138629e",
+    "url": "/D-Cloud/static/js/runtime-main.214682c5.js"
+  },
+  {
+    "revision": "211379fc5836313fe6ef697e7e8b2134",
+    "url": "/D-Cloud/static/media/box.211379fc.gif"
+  },
+  {
+    "revision": "659728a531adfa47529e65027c545f04",
+    "url": "/D-Cloud/static/media/noPreview.659728a5.png"
+  }
+]);
